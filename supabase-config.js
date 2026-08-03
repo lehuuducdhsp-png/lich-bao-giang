5eb6dfed883b5f11f3e21bd59a196dd61a79a483
lehuuducdhsp-png <lehuuducdhsp@gmail.com>
2026-08-03 10:22:16 +0700

    chore: add disabled Supabase runtime configuration

---
+++ supabase-config.js
@@ -0,0 +1,11 @@
+window.LBG_SUPABASE_CONFIG = Object.freeze({
+  enabled: false,
+  url: 'https://YOUR_PROJECT_REF.supabase.co',
+  publishableKey: 'sb_publishable_REPLACE_ME',
+  usernameDomain: 'users.lichbaogiang.internal',
+  bucket: 'tkb-private',
+  maxFileBytes: 50 * 1024 * 1024,
+  personalMaxFiles: 20,
+  personalMaxBytes: 500 * 1024 * 1024,
+  sharedPollMs: 60000
+});
