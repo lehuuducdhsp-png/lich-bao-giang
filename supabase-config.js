7e0b12433e4f425d21e64e8b54dadf2952b4bbb9
lehuuducdhsp-png <lehuuducdhsp@gmail.com>
2026-08-03 12:07:12 +0700

    test: enable Supabase only with auth-test query

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,8 @@
+const LBG_AUTH_TEST = new URLSearchParams(window.location.search).get('auth-test') === '1';
+
 window.LBG_SUPABASE_CONFIG = Object.freeze({
-  enabled: false,
+  enabled: LBG_AUTH_TEST,
+  testMode: LBG_AUTH_TEST,
   url: 'https://gmkibmybqfomypytmjxw.supabase.co',
   publishableKey: 'sb_publishable_x7E1lF0bK6qi6SJ1rrH8Eg_nzNHmtEg',
   usernameDomain: 'users.lichbaogiang.internal',
